fbe15e41b5f78aa8ca9a12af71bf448633c0c91d
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-12-02 18:16:42 +0530

    mutations done

---
+++ api/demo.js
@@ -0,0 +1,64 @@
+const gql = require("graphql-tag");
+const { ApolloServer } = require("apollo-server");
+
+const typeDefs = gql`
+  type User {
+    email: String!
+    avatar: String
+    friends: [User]!
+  }
+
+  type Shoe {
+    brand: String!
+    size: Int!
+  }
+
+  input ShoeInput {
+    brand: String
+    size: Int
+  }
+
+  input NewShoeInput {
+    brand: String!
+    size: Int!
+  }
+
+  type Query {
+    me: User!
+    shoes(input: ShoeInput): [Shoe]!
+  }
+
+  type Mutation {
+    newShoe(input: NewShoeInput!): Shoe!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    shoes(_, { input }) {
+      return [
+        { brand: "nike", size: "12" },
+        { brand: "adidas", size: "12" },
+      ].filter((shoe) => shoe.brand === input.brand);
+    },
+    me() {
+      return {
+        email: "yoda@masters.com",
+        avatar: "http://yoda.png",
+        friends: [],
+      };
+    },
+  },
+  Mutation: {
+    newShoe(_, { input }) {
+      return input;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen(4000).then(() => console.log("on port 4000"));
--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,25 @@
 
 module.exports = {
   Query: {
-    
+    pets(_, { input }, ctx) {
+      return ctx.models.Pet.findMany(input);
+    },
+    pet(_, { input }, ctx) {
+      return ctx.models.Pet.findOne(input);
+    },
   },
   Mutation: {
-    
+    newPet(_, { input }, ctx) {
+      const pet = ctx.models.Pet.create(input);
+      return pet;
+    },
   },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
-  },
-  User: {
-    
-  }
-}
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === "DOG"
+  //       ? "https://placedog.net/300/300"
+  //       : "http://placekitten.com/300/300";
+  //   },
+  // },
+  // User: {},
+};
